dd7bbb1ce520e1c9620910bf15343660ede6d972
Neo <killerkd@gmail.com>
2020-02-07 15:12:42 +0800

    first test

---
+++ JGPanda/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  JGPanda
+//
+//  Created by Neo Chou on 2020/2/7.
+//  Copyright © 2020 Neo Chou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Hello")
+        print("test")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
